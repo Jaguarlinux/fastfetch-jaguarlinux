aa51b4cfcc60a225508d289710d64edb254486f4
TigerClips1 <spongebob1966@proton.me>
2025-06-27 16:32:28 -0400

    Fix JaguarLinuxFastfetch
    Signed-off-by: TigerClips1 <spongebob1966@proton.me>

--- src/logo/builtin.c
+++ src/logo/builtin.c
@@ -2408,7 +2408,7 @@ static const FFlogo J[] = {
     // JaguarLinux
     {
         .names = {"JaguarLinux", "jaguar"},
-        .lines = FASTFETCH_DATATEXT_LOGO_JAGUAR,
+        .lines = FASTFETCH_DATATEXT_LOGO_JAGUARLINUX,
         .colors = {
             FF_COLOR_FG_DEFAULT,
             FF_COLOR_FG_BLACK,
